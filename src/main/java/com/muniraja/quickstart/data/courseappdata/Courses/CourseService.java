59b040d9731c31d22cff7b44bcf40e587c7c9000
MunirajaReddyB <bmunirajareddy582@gmail.com>
2021-02-21 00:08:42 +0530

    missed java files

---
+++ src/main/java/com/muniraja/quickstart/data/courseappdata/Courses/CourseService.java
@@ -0,0 +1,41 @@
+package com.muniraja.quickstart.data.courseappdata.Courses;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CourseService {
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+    public List<Course> getAllCourses(String topicId){
+
+        List<Course> courseList = new ArrayList<>();
+        courseRepository.findByTopicId(topicId).forEach(courseList::add);
+        return courseList;
+    }
+
+    public Optional<Course> getCourse(String id) {
+        return courseRepository.findById(id);
+    }
+
+    public void addCourse(Course course) {
+        //topics.add(topic);
+        courseRepository.save(course);
+    }
+
+    public void updateCourse(Course course) {
+
+        courseRepository.save(course);
+    }
+
+    public void deleteCourse(String id) {
+
+        courseRepository.deleteById(id);
+    }
+}
---
+++ src/test/java/com/muniraja/quickstart/data/couseappdata/CouseAppDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muniraja.quickstart.data.couseappdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouseAppDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
